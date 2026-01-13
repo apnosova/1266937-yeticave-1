4148a909ec0c1dca92544175275503ab0eaecfa8
angelina_n <angelinanosova@yandex.ru>
2026-01-13 03:18:56 +0300

    connect DB, add sql queries to get lots & categories

--- index.php
+++ index.php
@@ -1,22 +1,38 @@
 <?php
 
 /**
- * @var array $categories
- * @var array $lots
  * @var int $isAuth
  * @var string $userName
+ * @var mysqli $db
  */
 
-require_once __DIR__ . '/helpers.php';
-require_once __DIR__ . '/data.php';
+require_once __DIR__ . '/init.php';
 
-$pageContent = includeTemplate('main.php', ['categories' => $categories, 'lots' => $lots]);
-$layoutContent = includeTemplate('layout.php', [
-    'content' => $pageContent,
-    'categories' => $categories,
-    'isAuth' => $isAuth,
-    'userName' => $userName,
-    'title' => 'YetiCave - Главная страница'
-]);
+$isAuth = rand(0, 1);
+$userName = 'Angelina';
+
+$categories = getCategories($db);
+$lots = getLots($db);
+
+mysqli_close($db);
+
+$pageContent = includeTemplate(
+    'main.php',
+    [
+        'categories' => $categories,
+        'lots' => $lots
+    ]
+);
+
+$layoutContent = includeTemplate(
+    'layout.php',
+    [
+        'content' => $pageContent,
+        'categories' => $categories,
+        'isAuth' => $isAuth,
+        'userName' => $userName,
+        'title' => 'YetiCave - Главная страница'
+    ]
+);
 
 print ($layoutContent);
---
+++ init.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set("display_errors", "1");
+ini_set("display_startup_errors", "1");
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/functions/template.php';
+require_once __DIR__ . '/functions/helpers.php';
+require_once __DIR__ . '/functions/db.php';
+
+$config = require_once __DIR__ . '/config.php';
+
+$db = connectDB($config['db']);
